31443fcd6f70a79f2a0059571ed749dac9bc440b
Sand <sandboxasdf@gmail.com>
2024-10-17 13:06:45 +1100

    homepage reimplemented with js

--- src/homePage.js
+++ src/homePage.js
@@ -1,6 +1,11 @@
 import mainStageBanner from "./img/b01.jpg";
 import sloganBanner from "./img/bnr_sloganCrop.jpg";
 
+import i01 from"./img/01.jpg";
+import i02 from"./img/02.jpg";
+import i03 from"./img/03.jpg";
+import i04 from"./img/04.jpg";
+
 export default () => {
     
     const content = document.querySelector(".content");
@@ -31,8 +36,29 @@ function CreateMainSlogan(parentNode) {
 }
 
 function CreateMainSpecialty(parentNode) {
+    const specialtyArray = [
+        ['ミラノサンド', 'MILANO SANDWICH', i01],
+        ['ドリンク', 'DRINK', i02],
+        ['レモンヨーグルン', 'LEMON YOGHURT', i03],
+        ['桃のミルクレープ', 'MILK CREPE', i04],
+    ];
+    
     const SpecialtyDiv = AppendDivWithClasses(parentNode, ["main", "specialty"]);
         CreateSectionHeader(SpecialtyDiv, "おすすめ", "SPECIALTY");
+        const mainGrid = AppendDivWithClasses(SpecialtyDiv, ["mainContentGrid"]);
+            for (let i = 0; i < specialtyArray.length; i++) {
+                let item = specialtyArray[i];
+                CreateMainGridSquare(mainGrid, item[0], item[1], item[2]);
+            }
+}
+
+function CreateMainGridSquare(parent, title, subtitle, img) {
+    let mainGridSquare = AppendDivWithClasses(parent, ['mainContentGridSquare']);
+        let imageParent = AppendDivWithClasses(mainGridSquare, ["imageParent"]);
+            let imageParentBorder = AppendDivWithClasses(imageParent, ['imageParentBorder']);
+            let imageParentImg = AppendImg(imageParent, img);
+        AppendTag(mainGridSquare, 'p', title);
+        AppendTag(mainGridSquare, 'span', subtitle);
 }
 
 function CreateSectionHeader(parentNode, mainText, subText) {
